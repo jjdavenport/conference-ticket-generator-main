0fc946afc89d312ea20f5bb5f7ff7a5489b455b8
jjdavenport <jordanjamesdavenport@gmail.com>
2024-12-30 19:05:08 +0000

    fix: component name

--- src/components/controller.jsx
+++ src/components/controller.jsx
@@ -6,7 +6,7 @@ import Logo from "./logo";
 import CompletedTitle from "./completed-title";
 import Ticket from "./ticket";
 
-const PageOne = ({ input, setInput, setValid, valid }) => {
+const Controller = ({ input, setInput, setValid, valid }) => {
   return (
     <>
       <div className="flex h-full min-h-screen flex-col items-center gap-4 overflow-x-hidden bg-[url('../src/assets/pattern-squiggly-line-top.svg'),url('../src/assets/pattern-squiggly-line-bottom.svg'),url('../src/assets/pattern-lines.svg'),url('../src/assets/pattern-circle.svg'),url('../src/assets/pattern-circle.svg'),url('../src/assets/background-mobile.png')] bg-no-repeat font-custom text-neutral0 [background-position:top_2%_right,bottom_left_-10%,top_left,top_-4%_left_-10%,bottom_38%_right_-15%,center] [background-size:30%,80%,auto_70%,30%,25%,cover] md:[background-image:url('../src/assets/pattern-squiggly-line-top.svg'),url('../src/assets/pattern-squiggly-line-bottom.svg'),url('../src/assets/pattern-lines.svg'),url('../src/assets/pattern-circle.svg'),url('../src/assets/pattern-circle.svg'),url('../src/assets/background-tablet.png')] md:[background-position:top_10%_right,bottom_left,top_center,top_-8%_left_6%,bottom_48%_right_24%,center] md:[background-size:30%,57%,cover,12%,13%,cover] lg:[background-image:url('../src/assets/pattern-squiggly-line-top.svg'),url('../src/assets/pattern-squiggly-line-bottom.svg'),url('../src/assets/pattern-lines.svg'),url('../src/assets/pattern-circle.svg'),url('../src/assets/pattern-circle.svg'),url('../src/assets/background-desktop.png')]">
@@ -14,7 +14,6 @@ const PageOne = ({ input, setInput, setValid, valid }) => {
           <AnimatePresence mode="wait">
             {!valid ? (
               <motion.div
-                key="form"
                 initial={{ x: 0 }}
                 exit={{ x: "-100%" }}
                 transition={{ duration: 0.5 }}
@@ -26,7 +25,6 @@ const PageOne = ({ input, setInput, setValid, valid }) => {
               </motion.div>
             ) : (
               <motion.div
-                key="ticket"
                 initial={{ x: "100%" }}
                 animate={{ x: 0 }}
                 transition={{ duration: 0.5 }}
@@ -45,4 +43,4 @@ const PageOne = ({ input, setInput, setValid, valid }) => {
   );
 };
 
-export default PageOne;
+export default Controller;
